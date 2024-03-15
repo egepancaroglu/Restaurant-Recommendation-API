084a77d9f9b36c57a83c7f0f5b06eb79220f3505
egepancaroglu <egepancaroglu@gmail.com>
2024-03-15 04:58:08 +0300

    Added Config Server With Cloud Bus & Zipkin

---
+++ user-review-service/src/main/java/com/egepancaroglu/userreviewservice/controller/MessageController.java
@@ -0,0 +1,24 @@
+package com.egepancaroglu.userreviewservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author egepancaroglu
+ */
+
+@RefreshScope
+@RestController
+public class MessageController {
+
+    @Value("${spring.boot.message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String message() {
+        return message;
+    }
+
+}
